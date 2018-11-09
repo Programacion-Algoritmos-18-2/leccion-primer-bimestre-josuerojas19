9c832205c1a57d20cb3bef1c0b52d1cb051561be
Josue Rojas <jsebastianrojas19@gmail.com>
2018-11-09 11:40:55 -0500

    Segunda Pregunta

--- ejercicio1/mipaquete/modelo.py
+++ ejercicio1/mipaquete/modelo.py
@@ -32,3 +32,29 @@ class Empleado(object):
     def presentarDatos(self):
         cadena = "La informacion de %s %s\n\tcedula%s"%(self.obtenerNombre(), self.obtenerApellido(), self.obtenerCedula())
         return cadena
+
+class EmpleadoPorHoras(Empleado):
+        def __init__(self):
+            super(EmpleadoPorHoras, self). __init__()
+            self.numH = 0
+            self.valorH = 0
+
+        def agregarNumeroDeH(self, nh):
+            self.numH = nh
+
+        def obtenerNumeroDeH(self):
+            return self.numH
+
+        def agregarValorPorH(self, vh):
+            self.valorH = vh
+
+        def obtenerValorPorH(self):
+            return self.valorH
+
+        def calcular_Sueldo_Final(self):
+            sueldo = self.valorH * self.numH + self.comision_fija
+            return sueldo
+        def presentarDatos2(self):
+            cadena = "%s \n Numero horas: %s \n Valor Hora: %s \n Sueldo Final: %s" % (super(EmpleadoPorHoras, self).presentarDatos(), self.obtenerNumeroDeH(), self.obtenerValorPorH(), self.calcular_Sueldo_Final())
+            return cadena
+
